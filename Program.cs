3f35f6830d447e6a9050d4b652d30f329a13efc7
kailane <kailaneps@hotmail.com>
2020-07-17 11:18:05 -0300

    24_ObjetosArgumento finalizada

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _24_ObjetosArgumento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            
+            Carrinho c = new Carrinho();
+
+            Produto p1 = new Produto(1, "1984", 40.0f);
+            Produto p2 = new Produto(2, "A revolução dos bichos", 55.0f);
+            Produto p3 = new Produto(3, "Admirável Mundo Novo", 20.0f);
+            Produto p4 = new Produto(4, "O Conto da Aia", 30.0f);
+
+            c.Adicionar(p1);
+            c.Adicionar(p2);
+            c.Adicionar(p3);
+            c.Adicionar(p4);
+
+            c.Deletar(p2);
+
+            Produto prodAlterado = new Produto(1, "A menina que roubava livros", 25.0f);
+            
+
+            c.Ler();
+            c.MostrarTotal();           
+
+        }
+    }
+}
